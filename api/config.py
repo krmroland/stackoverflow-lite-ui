3e308ea0bc41dbaa670ff6ea8b8ee13ea82f1c47
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-08-26 16:16:06 +0300

    [Chore: finishes #160045670] Use dot_env files for environmental variables

--- api/config.py
+++ api/config.py
@@ -1,13 +1,27 @@
+from os import getenv, path
+from dotenv import load_dotenv
+import sys
+
+# load environment variables
+env_path = path.abspath(".env")
+
+if not path.exists(env_path):  # pragma: no cover
+    print("A .env configuration file was not found in your root project")
+    sys.exit(0)
+
+load_dotenv(verbose=True, dotenv_path=env_path)
+
+
 class Configuration:
     """Common configuration for all environments """
-    DB_NAME = "stackoverflow"
-    SECRET = "J0NJxenTRjjupTknRQsKFbka5JoIxgmAKQcMfl1vkB8"
+    DB_NAME = getenv("DB_NAME")
+    SECRET = getenv("APP_KEY")
 
 
 class TestConfiguration(Configuration):
     """Configuration for the test environment"""
     TESTING = True
-    DB_NAME = "testing_stackoverflow"
+    DB_NAME = getenv("TEST_DB_NAME")
 
 
 class DevelopmentConfiguration(Configuration):
